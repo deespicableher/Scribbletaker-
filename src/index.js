134023ecd5db828c5e1a164ecce01f09cbc35643
Doreen Kiprono <kipronodoreen2@gmail.com>
2022-03-09 14:02:01 -0500

    completed auth

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-Amplify.configure(config);
+
+Amplify. configure(config);
 
 
 ReactDOM.render(
